b10e944a998a43b05a99133a882c9a7cae70572c
null4848 <seungmin0055@gmail.com>
2025-06-19 17:49:20 +0900

    django

--- w0618/w01/pboard3/urls.py
+++ w0618/w01/pboard3/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name='pboard3'
 urlpatterns = [
     path('list/', views.list, name='list'),
-    path('view/<int:rank>/', views.view, name='view'),
+    path('searchAjax/', views.searchAjax, name='searchAjax'),
    
 ]
--- w0618/w01/pboard3/views.py
+++ w0618/w01/pboard3/views.py
@@ -1,29 +1,51 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 import requests
 import json
 
-dlist = []
-# Create your views here.
-def list(request):
-    global dlist
-    key = '73f817d94dff0faf42032fd8f09bb219'
-    url = f'https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key={key}&targetDt=20250617'
-    response = requests.get(url)
-    json_data = json.loads(response.text)
-    dlist = json_data['boxOfficeResult']['dailyBoxOfficeList']
-    print("10개 : ",dlist )
-    context = {"list":dlist}
-    return render(request, 'pboard3/list.html', context)
+### 전역변수
+dlist = []  # list함수에서 공공데이터를 가지고 와서 view함수에 전달
 
-def view(request, rank):
-    global dlist
-    print("넘어온 rank", rank)
+## ajax통신 - 리턴타입 JsonResponse
+def searchAjax(request):
+    ## 영화데이터 가져오기
+    targetDt = request.POST.get('searchInput','20250617')
+    print('targetDt : ',targetDt)
+    # 공공데이터 가져오기에 필요한 정보
+    key = 'b4cefdc91025f56609b0e03df7a460a0'
+    # targetDt = '20250617'
+    url = f'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key={key}&targetDt={targetDt}'
+     # 공공데이터 가져오기
+    response = requests.get(url)          # 공공데이터 가져온 타입 : str타입
+    json_data = json.loads(response.text)  # json타입으로 변경 -> dict타입
+    # 필요한 데이터, 리스트로 변경해서 전달
+    dlist = json_data['boxOfficeResult']['dailyBoxOfficeList']
+    print("10개 : ",dlist)
+    context = {'list':dlist}
+    return JsonResponse(context) 
     
-    context = {}
-    for d in dlist:
-        if d['rank']==str(rank):
-            context['dData']=d
-            
-    print(context['dData'])
-        
-    return render(request, 'pboard3/view.html', context)
+
+
+# 공공데이터 리스트
+def list(request):
+    # 공통영역 : 영화데이터 호출
+    context = publicScreen('20250617')
+    return render(request,'pboard3/list.html',context)
+
+
+## 공통영역 : 영화데이터 가져오기 함수
+def publicScreen(targetDt):
+    global dlist #전역변수 사용
+    # 공공데이터 가져오기에 필요한 정보
+    key = 'b4cefdc91025f56609b0e03df7a460a0'
+    targetDt = '20250617'
+    url = f'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key={key}&targetDt={targetDt}'
+     # 공공데이터 가져오기
+    response = requests.get(url)          # 공공데이터 가져온 타입 : str타입
+    json_data = json.loads(response.text)  # json타입으로 변경 -> dict타입
+    # 필요한 데이터, 리스트로 변경해서 전달
+    dlist = json_data['boxOfficeResult']['dailyBoxOfficeList']
+    print("10개 : ",dlist)
+    
+    context = {'list':dlist}
+    return context
